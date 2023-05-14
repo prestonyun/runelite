b521be28217733fbf34ca9d46cf705702ea32ea7
Preston Yun <prestonyun32@gmail.com>
2023-05-13 18:03:53 -0700

    my status socket

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/mystatussocket/MyStatusSocketEndpoints.java
@@ -0,0 +1,11 @@
+package net.runelite.client.plugins.mystatussocket;
+
+public class MyStatusSocketEndpoints
+{
+
+    public static String LOGIN_ENDPOINT = "login";
+
+    public static String LOG_ENDPOINT = "log";
+    public static String ACTION_API = "action";
+
+}
